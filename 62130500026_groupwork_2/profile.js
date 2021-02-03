033ea6cb120a38e118ed7055b78605b5c7c77062
JeongChedah <donlawan.13@mail.kmutt.ac.th>
2021-02-03 21:00:21 +0700

    [ADD] groupwork_2

---
+++ 62130500026_groupwork_2/profile.js
@@ -0,0 +1,12 @@
+  const app = {
+      data() {
+          return {
+              image: './images/Me.JPG',
+              name: 'Donlawan Petcharat',
+              ID: 'ID:62130500026'
+
+          }
+      },
+
+  }
+  Vue.createApp(app).mount('#app')
